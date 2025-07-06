b72061b728c9362fa92877a053d894442da91975
ILLYA SMYK <smykillya@gmail.com>
2025-07-06 17:45:02 +0200

    2

--- js/task-5.js
+++ js/task-5.js
@@ -4,10 +4,13 @@ function getRandomHexColor() {
     .padStart(6, 0)}`;
 }
 
-//Напиши скрипт, який змінює колір фону елемента <body> через інлайн-стиль по кліку на button.change-color і задає це значення кольору текстовим вмістом для span.color.
+const ButtonEl = document.querySelector(".change-color");
+const output = document.querySelector(".color");
 
-//Для генерування випадкового кольору використовуй функцію getRandomHexColor().
+const onButton = (e) => {
+  const color = getRandomHexColor();
+  document.body.style.backgroundColor = color;
+  output.textContent = color;
+};
 
-//Фон на <body> задається тільки після кліку на button.change-color
-//При кожному кліку на елемент button.change-color фон <body> зафарбовується новим рандомним кольором
-//На <body> і span.color значення одного й того самого кольору
+ButtonEl.addEventListener("click", onButton);
